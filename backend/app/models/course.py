61bfb9c7b69fd076ae626453b79c43b7fb94460a
HoangPhuc <hoangphuc982004@gmail.com>
2026-04-14 16:04:58 +0700

    OC-74 BE Xem thông tin khoá học

--- backend/app/models/course.py
+++ backend/app/models/course.py
@@ -1,5 +1,5 @@
+# app/models/course.py
 from app.configs.db import db
-
 
 class Course(db.Model):
     __tablename__ = 'courses'
@@ -8,3 +8,17 @@ class Course(db.Model):
     description = db.Column(db.Text)
     price = db.Column(db.Float)
     instructor_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+
+    # Kết nối với bảng User
+    instructor = db.relationship('User', backref='courses')
+    # Kết nối với bảng Courses
+    lessons = db.relationship('Lesson', backref='course', lazy=True)
+    def to_dict(self):
+        return {
+            "course_id": self.course_id,
+            "title": self.title,
+            "description": self.description,
+            "price": self.price,
+            "instructor_id": self.instructor_id,
+            "instructor_name": self.instructor.name if self.instructor else "Unknown"
+        }
--- backend/app/routes/course_routes.py
+++ backend/app/routes/course_routes.py
@@ -1,7 +1,27 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request
+from app.models import Course
+from app.services.course_service import get_courses_service, get_course_detail_service
 
 course_bp = Blueprint('course_bp', __name__)
 
 @course_bp.route('/', methods=['GET'])
 def get_courses():
-    return {"message": "List of courses"}
+
+    page = request.args.get('page', 1, type=int)
+    size = min(request.args.get('size', 10, type=int), 50)
+
+    keyword = request.args.get('q')
+    sort = request.args.get('sort', 'id')
+
+    data = get_courses_service(page, size, keyword, sort)
+
+    return jsonify(data), 200
+
+@course_bp.route('/<int:course_id>', methods=['GET'])
+def get_course_detail(course_id):
+    data = get_course_detail_service(course_id)
+
+    if not data:
+        return jsonify({"message": "Course not found"}), 404
+
+    return jsonify(data), 200
